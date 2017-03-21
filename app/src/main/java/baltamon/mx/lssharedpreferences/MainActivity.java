0afa87488bbe6c95dfb31411bd68a9824c9cd265
Balthair94 <archerbaltazar@gmail.com>
2017-03-21 02:31:08 -0600

    It is possible to edit the user information and close the session

--- app/src/main/java/baltamon/mx/lssharedpreferences/MainActivity.java
+++ app/src/main/java/baltamon/mx/lssharedpreferences/MainActivity.java
@@ -1,12 +1,16 @@
 package baltamon.mx.lssharedpreferences;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import baltamon.mx.lssharedpreferences.activities.RegisterActivity;
 import baltamon.mx.lssharedpreferences.persistence.UserSession;
 import baltamon.mx.lssharedpreferences.viewholders.RegisterViewHolder;
 
@@ -24,10 +28,41 @@ public class MainActivity extends AppCompatActivity {
         userSession = new UserSession(getApplicationContext());
         holder = new RegisterViewHolder(this.findViewById(android.R.id.content));
         showUserData();
+        setUpButtons();
+    }
+
+    private void setUpButtons() {
+        Button btnUpdate = (Button) findViewById(R.id.btn_update);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUserData();
+            }
+        });
+
+        Button btnLogut = (Button) findViewById(R.id.btn_logout);
+        btnLogut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                userSession.logout();
+                Toast.makeText(getApplicationContext(), "Logout", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+                finish();
+            }
+        });
+    }
+
+    private void updateUserData(){
+        userSession.setUserName(holder.getmUserName().getText().toString());
+        userSession.setUserEmail(holder.getmUserEmail().getText().toString());
+        userSession.setUserPhone(holder.getmUserPhone().getText().toString());
+        Toast.makeText(this, "Data updated", Toast.LENGTH_SHORT).show();
     }
 
     private void showUserData() {
-
+        holder.getmUserName().setText(userSession.getUserName());
+        holder.getmUserEmail().setText(userSession.getUserEmail());
+        holder.getmUserPhone().setText(userSession.getUserPhone());
     }
 
     private void setUpToolbar() {
